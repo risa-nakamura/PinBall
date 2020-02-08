e85075ae4ba99cead265d4b7cdc8f0725a3fa94d
risa-nakamura <negi39390822nasi@gmail.com>
2020-02-08 15:04:52 +0900

    PinBall
    PinBallGameの得点表示課題まで

---
+++ Assets/Score.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    private GameObject ScoreText;
+    public int point = 0;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //point = 0;
+        this.ScoreText = GameObject.Find("ScoreText");
+
+    }
+     void OnCollisionEnter(Collision collision)
+    {
+        string Tag = collision.gameObject.tag;
+       
+
+
+        if (Tag == "LargeStarTag")
+        {
+            point += 20;
+        }
+        else if (Tag == "SmallStarTag")
+        {
+            point += 10;
+        }
+        
+        else if (Tag == "SmallCloudTag")
+        {
+            point += 15;
+        }
+        else if (Tag == "LargeCloudTag")
+        {
+            point += 30;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        this.ScoreText.GetComponent<Text>().text = ("Score:" +point);
+        //Debug.Log("Score:" + point);
+    }
+}
