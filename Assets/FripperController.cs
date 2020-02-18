4b93cd23f0b21f43028c4dcc7754154ede683ddd
risa-nakamura <negi39390822nasi@gmail.com>
2020-02-18 22:42:46 +0900

    タッチ操作の追加
    タッチ操作出来るようにしました。
    カメラの位置の調整。

--- Assets/FripperController.cs
+++ Assets/FripperController.cs
@@ -9,6 +9,7 @@ public class FripperController : MonoBehaviour {
     private float defaultAngle = 20;
     private float flickAngle = -20;
 
+    
 
 	// Use this for initialization
 	void Start () {
@@ -35,6 +36,43 @@ public class FripperController : MonoBehaviour {
         {
             SetAngle(this.defaultAngle);
         }
+
+        //指の数の取得
+        var touchCount = Input.touchCount;
+    　　//タッチの数ループ
+        for(var i = 0; i < touchCount; i++)
+        {
+                var touch = Input.GetTouch(i);
+            if (Input.mousePosition.x >= Screen.width / 2)
+            {
+
+
+
+                if (touch.phase == TouchPhase.Began && tag == "RightFripperTag")
+                {
+
+                    SetAngle(this.flickAngle);
+                }
+                if (touch.phase == TouchPhase.Ended && tag == "RightFripperTag")
+                {
+                    SetAngle(this.defaultAngle);
+                }
+            }
+            else if(Input.mousePosition.x <= Screen.width / 2)
+            {
+                if(touch.phase == TouchPhase.Began && tag == "LeftFripperTag")
+                {
+                    SetAngle(this.flickAngle);
+                }
+                if(touch.phase == TouchPhase.Ended && tag == "LeftFripperTag")
+                {
+                    SetAngle(this.defaultAngle);
+                }
+            }
+
+        }
+
+       
 	}
 
     public void SetAngle (float angle)
@@ -44,3 +82,4 @@ public class FripperController : MonoBehaviour {
         this.myHingeJoint.spring = jointSpr;
     }
 }
+            
